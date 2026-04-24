4c0a7a224a00e806e82bf66b96a8f5fd5658ee34
cparrybr <cparry@brookings.edu>
2026-04-24 12:51:14 -0400

    i make a change
    no point in having scrapers if they don't run, they gotta run

--- src/scraper/sites/fed_atlanta_scraper.py
+++ src/scraper/sites/fed_atlanta_scraper.py
@@ -1,65 +1,51 @@
-from ..generic_scraper import GenericScraper
 import feedparser
-import calendar
+from datetime import datetime
+from ..generic_scraper import GenericScraper
+
 
 class FedAtlantaScraper(GenericScraper):
     def __init__(self):
-        super().__init__(source = 'FED-ATLANTA')
+        super().__init__(source="FED-ATLANTA")
 
-    # Public method which is called from outside the class.
     def fetch_data(self):
-        '''
-        Requests and parses the source's main RSS feed using feedparser
-        to get title, link, author, date, number, and abstract for
-        each working paper entry.
+        f = feedparser.parse("https://www.atlantafed.org/rss/wps")
 
-        :return: A list of dictionaries containing Title, Author, Link, 
-        Abstract, Number and Date for each working paper entry 
-        :rtype: list
-        '''
-        # RSS feed URL
-        url = "https://www.atlantafed.org/rss/wps"
-        # Request and parse RSS feed contents
-        f = feedparser.parse(url)
-
-        # Initialize output `data`
         data = []
         for entry in f.entries:
-            title = entry.title
+            try:
+                if not getattr(entry, "published_parsed", None):
+                    continue
 
-            link = entry.link
+                dt = datetime(*entry.published_parsed[:6])
+                date_str = dt.strftime("%Y-%m-%d")
+                month_year = dt.strftime("%B %Y")
 
-            # The date is the published entry, minus 11 characters (indicating hour, minute, and time zone)
-            date = entry.published[:-10].strip()
-            # The last 4 chars of date comprise the year (helper variable)
-            year = date[-4:] # year is a helper variable
-            mon = date[-8:-5] # mon is a helper variable (3-letter month name)
-            # helper variable
-            month_year = calendar.month_name[list(calendar.month_abbr).index(mon)] + " " + year 
+                abstract = (
+                    entry.description.split(month_year)[1].strip()
+                    if month_year in entry.description
+                    else ""
+                )
+                author = entry.description.split("Working Paper")[0].strip()
 
-            # The abstract is the 1st element of "description", after splitting it by the 
-            # name of the month and year the paper was published. This is because the
-            # "desciption" also contains author and working paper number. This was the
-            # simplest way I could find to isolate the abstract without navigating to different
-            # URLs.
-            abstract = entry.description.split(f"{month_year}")[1]
+                try:
+                    number = (
+                        entry.description
+                        .split("Working Paper ")[1]
+                        .split(month_year)[0]
+                        .strip()
+                    )
+                except (IndexError, ValueError):
+                    number = entry.link.rstrip("/").split("/")[-1]
 
-            # The number is the middle element of "description", after splitting it by the
-            #  string "Working Paper" and by the month_year string.
-            number = entry.description.split("Working Paper ")[1].split(f"{month_year}")[0].strip()
+                data.append({
+                    "Title": entry.title,
+                    "Link": entry.link,
+                    "Date": date_str,
+                    "Abstract": abstract,
+                    "Author": author,
+                    "Number": number,
+                })
+            except Exception as e:
+                print(f"[FED-ATLANTA] entry error: {e}")
 
-            # The author is the first element of "description", the part that appears just
-            # before the string "Working Paper".
-            author = entry.description.split("Working Paper")[0].strip()
-
-            # Append title, link, date, abstract, number, and author
-            # `data` dictionary list
-            data.append({'Title': title,
-                    'Link': link,
-                    'Date': date,
-                    'Abstract': abstract,
-                    'Number': number,
-                    'Author': author
-                    })
-            
         return data
